d9163f326d070c962e8cc428d365f914e2ec06a3
mohamed shegow <shegowmo@gmail.com>
2023-03-05 00:06:03 +0100

    omniAuth route added

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,13 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :omniauthable, omniauth_providers: [:google_oauth2]
+
+  def self.from_omniuath(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+    end
+  end
+
+
 end
