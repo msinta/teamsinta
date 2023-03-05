bf1f769a5c542f4457f19ce2d67ed692097950bb
Mattias Velamsson <mattiaswelamsson@gmail.com>
2023-03-06 06:31:15 +0900

    update default route to components index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'components/index'
-  root 'pages#home'
+  # get 'components/index'
+  root 'components#index'
   devise_for :users, controllers: {
     registrations: 'users/registrations',
     sessions: 'users/sessions',
