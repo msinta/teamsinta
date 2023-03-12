6205ea2fe37e4a588f767b6060c9a5ca6523dc5f
mohamed shegow <shegowmo@gmail.com>
2023-03-12 13:20:34 +0100

    controller actions added, working on testing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,20 @@
 class UsersController < ApplicationController
+
+  def show
+    @user = User.find(:id)
+  end
+
+  def index
+    @users = User.all
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
 end
